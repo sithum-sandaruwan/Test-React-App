1c95b32ed158963d379cd46a15e80b6d277d1421
Sithum <sithumsandaruwan200123@gmail.com>
2024-10-02 01:09:19 +0530

    Initial commit

---
+++ test-app/src/App.jsx
@@ -0,0 +1,15 @@
+import Footer from "./Footer";
+import Header from "./Header";
+
+function App() {
+
+  return (
+    <>
+      <Header />
+      <Footer />
+    </>
+
+  );
+}
+
+export default App
