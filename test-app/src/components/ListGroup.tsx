ffede64988331648208583d63112fffe620800a1
Sithum <sithumsandaruwan200123@gmail.com>
2024-10-04 12:59:06 +0530

    click handling created

--- test-app/src/components/ListGroup.tsx
+++ test-app/src/components/ListGroup.tsx
@@ -1,12 +1,16 @@
+import { MouseEvent } from "react";
 function ListGroup() {
 
-    const items = ["Item 1", "Item 2", "Item 3"];
+    let items = ["Item 1", "Item 2", "Item 3"];
+
+    const handleClick =  (event : MouseEvent) => console.log(event)
     return (
         <>
             <h1>List Group</h1>
-
+            {items.length === 0 && <p>No Item Found</p>}
             <ul className="list-group">
-                {items.map(item => <li key={item}>{item}</li>)}
+                {items.map(item => (<li className="list-group-item" key={item}
+                    onClick={handleClick}> {item}</li>))}
             </ul>
         </>
 
